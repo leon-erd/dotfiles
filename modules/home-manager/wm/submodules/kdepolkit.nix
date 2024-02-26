68b41c893edcad43eb107a1aa51100e04fda4eeb
Leon <leon.erd@student.uibk.ac.at>
2024-02-26 18:37:43 +0100

    better way to start kde polkit

--- modules/home-manager/wm/submodules/kdepolkit.nix
+++ modules/home-manager/wm/submodules/kdepolkit.nix
@@ -5,7 +5,7 @@
 
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "/nix/store/$(ls -la /nix/store | grep polkit-kde-agent | grep '^d' | awk '{print $9}' | head -n 1)/libexec/polkit-kde-authentication-agent-1"
+      "${pkgs.libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1"
     ];
   };
 }
