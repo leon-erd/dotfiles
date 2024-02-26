d6e52185ddc7ba4a56b78508cbbe80619b3243eb
Leon <leon.erd@student.uibk.ac.at>
2024-02-26 15:04:26 +0100

    overrite nixpkgs swaync with new version

--- modules/home-manager/wm/submodules/swaync/swaync.nix
+++ modules/home-manager/wm/submodules/swaync/swaync.nix
@@ -1,7 +1,29 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.swaynotificationcenter ];
+  home.packages = [
+    (pkgs.swaynotificationcenter.overrideAttrs (previousAttrs:  rec {
+      version = "0.10.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "ErikReider";
+        repo = previousAttrs.pname;
+        rev = "v${version}";
+        hash = "sha256-7O+DX4uuncUqx5zEKQprZE6tctteT6NU01V2EBHiFqA=";
+      };
+      patches = [];
+      postPatch = ''
+        chmod +x build-aux/meson/postinstall.py
+        patchShebangs build-aux/meson/postinstall.py
+      '';
+      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [
+        pkgs.sassc
+      ];
+      buildInputs = previousAttrs.buildInputs ++ [
+        pkgs.gvfs
+        pkgs.pantheon.granite
+      ];
+    }))
+  ];
   wayland.windowManager.hyprland.settings = {
     bind = [ "$mainMod, N, exec, swaync-client -t -sw" ];
     exec-once = [ "swaync" ];
