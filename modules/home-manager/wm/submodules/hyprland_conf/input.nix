0088436caac44d2715776e8bfe8e6081d324d937
Leon <leon.erd@student.uibk.ac.at>
2024-02-29 14:48:00 +0100

    update flake.lock and fix stylus pen bound to laptop screen

--- modules/home-manager/wm/submodules/hyprland_conf/input.nix
+++ modules/home-manager/wm/submodules/hyprland_conf/input.nix
@@ -9,24 +9,14 @@
         natural_scroll = true;
         drag_lock = true;
       };
-
-
-    # All the below is me trying to map the pen to laptop screen
-      tablet = {
-        output = "eDP-1";
-      };
-
-      touchdevice = {
-        output = "eDP-1";
-      };
     };
 
-    "device:cust0000:00-04f3:2a4a-stylus" = {
+    device = {
+      name = "cust0000:00-04f3:2a4a-stylus";
       output = "eDP-1";
     };
 
     gestures = {
-      # See https://wiki.hyprland.org/Configuring/Variables/ for more
       workspace_swipe = true;
       workspace_swipe_forever = true;
     };
