8e5a206813953735c266574a77027326ac53d9f2
Leon <leon.erd@student.uibk.ac.at>
2024-03-18 12:46:05 +0100

    edit hyprlock.nix

--- modules/home-manager/wm/submodules/hyprlock.nix
+++ modules/home-manager/wm/submodules/hyprlock.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, ... }:
 let
   #walColor = (builtins.fromJSON (builtins.readFile ./colors-rgb.json)).colors.color1;
-  labelColor = "rgba(0, 0, 0, 0.8)";
+  labelColor = "rgba(255, 255, 255, 1)";
 in
 {
   imports = [
@@ -15,15 +15,15 @@ in
     general.grace = 3;
     backgrounds = [
       {
-#         path = "/home/leon/scripts/wallpaper/wallpaper.png";
-        path = "screenshot";
+        path = "~/scripts/wallpaper/wallpaper.png";
+#         path = "screenshot";
         blur_size = 4;
         blur_passes = 4;
       }
     ];
     labels = [
       {
-        text = ''cmd[update:500] echo "<b>$(date +'%H:%M:%S')</b>"'';
+        text = ''cmd[update:500] echo "$(date +'%H:%M:%S')"'';
         color = labelColor;
         font_size = 50;
         font_family = "NotoMonoNerdFont";
@@ -40,6 +40,7 @@ in
         text = ''󱁕 Hey <span foreground="##880000" style="italic">$USER</span> 󱁕'';
         color = labelColor;
         position = {x=0; y=30;};
+        font_family = "NotoSansNerdFont";
       }
       {
         text = "";
@@ -55,10 +56,11 @@ in
         dots_spacing = 0.3;
         outer_color = "rgba(0, 0, 0, 0)";
         inner_color = "rgba(0, 0, 0, 0)";
-        font_color = "rgba(255, 255, 255, 0.5)";
+        font_color = "rgba(255, 255, 255, 1)";
         fade_on_empty = false;
         hide_input = false;
         position = {x=0; y=-30;};
+        placeholder_text = ''<span foreground="##FFFFFF80"><i>Input Password...</i></span>'';
       }
     ];
   };
