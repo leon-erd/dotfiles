eb8742d03a420602a860811d2dc782bdea1b0353
Leon <leon.erd@student.uibk.ac.at>
2024-03-20 13:58:27 +0100

    update hyprlock.nix (add shadows, make keybind lock immediately, better colors)

--- modules/home-manager/wm/submodules/hyprlock.nix
+++ modules/home-manager/wm/submodules/hyprlock.nix
@@ -28,6 +28,7 @@ in
         font_size = 50;
         font_family = "NotoMonoNerdFont";
         position = {x=0; y=250;};
+        shadow_passes = 1;
       }
       {
         text = ''cmd[update:10000] echo "$(date +'%a %d. %b')"'';
@@ -35,18 +36,21 @@ in
         font_size = 15;
         font_family = "NotoSansNerdFont";
         position = {x=0; y=330;};
+        shadow_passes = 1;
       }
       {
         text = ''󱁕 Hey <span foreground="##880000" style="italic">$USER</span> 󱁕'';
         color = labelColor;
         position = {x=0; y=30;};
         font_family = "NotoSansNerdFont";
+        shadow_passes = 1;
       }
       {
         text = "";
         color = labelColor;
         position = {x=0; y=80;};
         valign = "center";
+        shadow_passes = 1;
       }
     ];
 
@@ -58,15 +62,17 @@ in
         inner_color = "rgba(0, 0, 0, 0)";
         font_color = "rgba(255, 255, 255, 1)";
         fade_on_empty = false;
-        hide_input = false;
-        position = {x=0; y=-30;};
         placeholder_text = ''<span foreground="##FFFFFF80"><i>Input Password...</i></span>'';
+        hide_input = false;
+        fail_text = ''<span foreground="##FFFFFF"><i>$FAIL <b>($ATTEMPTS)</b></i></span>'';
+        capslock_color = "rgba(130, 0, 255, 1)";
+        position = {x=0; y=-30;};
       }
     ];
   };
 
   wayland.windowManager.hyprland.settings = {
-    bindr = [ "$mainMod, L, exec, hyprlock" ];
+    bindr = [ "$mainMod, L, exec, hyprlock --immediate" ];
   };
 }
 
