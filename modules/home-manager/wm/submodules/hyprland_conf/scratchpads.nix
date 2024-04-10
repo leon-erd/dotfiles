91e1a8e37f4116915756600a7550f218f72f8313
Leon <leon.erd@student.uibk.ac.at>
2024-04-10 15:44:29 +0200

    hyprland scratchpads: add htop and btop

--- modules/home-manager/wm/submodules/hyprland_conf/scratchpads.nix
+++ modules/home-manager/wm/submodules/hyprland_conf/scratchpads.nix
@@ -3,10 +3,14 @@
 {
   wayland.windowManager.hyprland.settings = {
     workspace = [
+      "special:btop, on-created-empty:[fullscreen] alacritty -e btop"
+      "special:htop, on-created-empty:[fullscreen] alacritty -e htop"
       "special:spotify, on-created-empty:[fullscreen] spotify"
       "special:terminal, on-created-empty:[fullscreen] alacritty"
     ];
     bind = [
+      "$mainMod, B, togglespecialworkspace, btop"
+      "$mainMod, H, togglespecialworkspace, htop"
       "$mainMod, S, togglespecialworkspace, spotify"
       "$mainMod, T, togglespecialworkspace, terminal"
     ];
