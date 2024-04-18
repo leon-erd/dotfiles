12f06c9947146863322278e3e2d2c6a5443987e7
Leon <leon.erd@student.uibk.ac.at>
2024-04-19 01:05:52 +0200

    hyprland: edit workspacerules

--- modules/home-manager/wm/submodules/hyprland_conf/workspacerules.nix
+++ modules/home-manager/wm/submodules/hyprland_conf/workspacerules.nix
@@ -4,8 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     workspace = [
       "2, on-created-empty:dolphin"
-      "5, monitor:HDMI-A-1, default:true, on-created-empty:firefox"
-      "9, monitor:HDMI-A-1"
+      "5, on-created-empty:firefox"
     ];
   };
 }
