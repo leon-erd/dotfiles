236909dbb242c463bd5fe1e80acba90da762fb6b
Leon <leon.erd@student.uibk.ac.at>
2024-04-23 15:46:04 +0200

    tofi: home manager program

--- modules/home-manager/wm/submodules/tofi.nix
+++ modules/home-manager/wm/submodules/tofi.nix
@@ -1,7 +1,28 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  home.packages = [ pkgs.tofi ];
+  programs.tofi = {
+    enable = true;
+    settings = {
+      anchor = "top";
+      width = "100%";
+      height = 36;
+      horizontal = true;
+      font-size = 14;
+      prompt-text = " run: ";
+      outline-width = 0;
+      border-width = 0;
+      background-color = "#00000099";
+      min-input-width = 120;
+      result-spacing = 15;
+      padding-top = 5;
+      padding-bottom = 0;
+      padding-left = 0;
+      padding-right = 0;
+      font = "/run/current-system/sw/share/X11/fonts/NotoSansNerdFont-Regular.ttf";
+    };
+  };
+
   wayland.windowManager.hyprland.settings = {
     bindr = [ "$mainMod, $mainMod_L, exec, tofi-drun --drun-launch=true" ];
     exec-once = [ "rm ~/.cache/tofi-drun" ];
@@ -10,22 +31,4 @@
       "animation slide, launcher"
     ];
   };
-  xdg.configFile."tofi/config".text = ''
-    anchor = top
-    width = 100%
-    height = 36
-    horizontal = true
-    font-size = 14
-    prompt-text = " run: "
-    outline-width = 0
-    border-width = 0
-    background-color = #00000099
-    min-input-width = 120
-    result-spacing = 15
-    padding-top = 5
-    padding-bottom = 0
-    padding-left = 0
-    padding-right = 0
-    font = /run/current-system/sw/share/X11/fonts/NotoSansNerdFont-Regular.ttf
-  '';
 }
