2f906e40094a029a86c321cc7a631c6386c8051f
Leon <leon.erd@student.uibk.ac.at>
2024-04-23 11:21:51 +0200

    hyprland conf: remove some comments

--- modules/home-manager/wm/submodules/hyprland_conf/monitors.nix
+++ modules/home-manager/wm/submodules/hyprland_conf/monitors.nix
@@ -2,11 +2,6 @@
 
 {
   wayland.windowManager.hyprland.settings = {
-    # Monitor setup:
-    # Usually handled by kanshi
-    # You can also use nwg-displays to arrange monitors (you then have to source monitors.conf)
-    # source = ["~/.config/hypr/monitors.conf"];
-
     # Fallback rule
     monitor = ", preferred, auto, 1";
   };
