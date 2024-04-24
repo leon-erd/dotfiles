f3cf60ac615f34cd26bfe4754f5ba56711648f95
Leon <leon.erd@student.uibk.ac.at>
2024-04-24 14:06:37 +0200

    nm-applet, blueman-applet: move into their own modules

--- modules/home-manager/wm/submodules/hyprland_conf/exec-once.nix
+++ modules/home-manager/wm/submodules/hyprland_conf/exec-once.nix
@@ -4,8 +4,6 @@
   wayland.windowManager.hyprland.settings = {
     # Execute at launch
     exec-once = [
-      "nm-applet --indicator"
-      "blueman-applet"
       "sleep 5 && nextcloud --background"
       "~/scripts/hibernate_on_low_battery.sh"
     ];
