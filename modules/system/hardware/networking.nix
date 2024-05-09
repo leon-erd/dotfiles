184b46652a4820936f1ac67d94b2fcedc0981bb0
Leon <leon.erd@student.uibk.ac.at>
2024-05-09 13:19:12 +0200

    wireguard: fix

--- modules/system/hardware/networking.nix
+++ modules/system/hardware/networking.nix
@@ -6,4 +6,6 @@
   networking.networkmanager.enable = true;
   environment.systemPackages = [ pkgs.networkmanagerapplet ];
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  services.resolved.enable = true; # needed for wireguard
 }
