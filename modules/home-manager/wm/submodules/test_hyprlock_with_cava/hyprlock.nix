231c39b4d06b07f5a804ee08f59c26231ea0b452
Leon <leon.erd@student.uibk.ac.at>
2024-06-18 17:33:42 +0200

    play around with "hyprlock_with_cava" + rename "test:" -> "test_"

---
+++ modules/home-manager/wm/submodules/test_hyprlock_with_cava/hyprlock.nix
@@ -0,0 +1,122 @@
+{ pkgs, inputs, ... }:
+let
+  #walColor = (builtins.fromJSON (builtins.readFile ./colors-rgb.json)).colors.color1;
+  labelColor = "rgba(255, 255, 255, 1)";
+in
+{
+  programs.hyprlock = {
+    enable = true;
+    package = inputs.hyprlock.packages.${pkgs.system}.default;
+    settings = {
+      general = {
+        grace = 3;
+        hide_cursor = true;
+      };
+      background = [
+        {
+          monitor = "HDMI-A-1";
+          path = "~/scripts/wallpaper/wallpaper.jpg";
+          blur_size = 4;
+          blur_passes = 4;
+        }
+        {
+          monitor = "DP-2";
+          path = "~/scripts/wallpaper/wallpaper.jpg";
+          blur_size = 4;
+          blur_passes = 4;
+        }
+        {
+          monitor = "eDP-1";
+          color = "rgba(0, 0, 0, 0)";
+          path = "";
+          blur_passes = 0;
+        }
+      ];
+      input-field = [
+        {
+          placeholder_text = ''<span foreground="##FFFFFF80"><i>Input Password...</i></span>'';
+          size = "200, 50";
+          outline_thickness = 1;
+          dots_spacing = 0.3;
+          inner_color = "rgba(0, 0, 0, 0)";
+          outer_color = "rgba(0, 0, 0, 0)";
+          font_color = "rgba(255, 255, 255, 1)";
+          capslock_color = "rgba(130, 0, 255, 1)";
+          fade_on_empty = false;
+          hide_input = false;
+          fail_text = ''<span foreground="##FFFFFF"><i>$FAIL <b>($ATTEMPTS)</b></i></span>'';
+          position = "0, -30";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+      label = [
+        {
+          text = ''cmd[update:500] echo "$(date +'%H:%M:%S')"'';
+          font_size = 50;
+          font_family = "NotoMonoNerdFont";
+          color = labelColor;
+          position = "0, 250";
+          halign = "center";
+          valign = "center";
+          shadow_passes = 1;
+        }
+        {
+          text = ''cmd[update:10000] echo "$(date +'%a %d. %b')"'';
+          font_size = 15;
+          font_family = "NotoSansNerdFont";
+          color = labelColor;
+          position = "0, 330";
+          halign = "center";
+          valign = "center";
+          shadow_passes = 1;
+        }
+        {
+          text = ''󱁕 Hey <span foreground="##880000" style="italic">$USER</span> 󱁕'';
+          font_size = 25;
+          font_family = "NotoSansNerdFont";
+          color = labelColor;
+          position = "0, 30";
+          halign = "center";
+          valign = "center";
+          shadow_passes = 1;
+        }
+        {
+          text = ''<span> </span>'';
+          font_size = 25;
+          color = labelColor;
+          position = "0, 80";
+          halign = "center";
+          valign = "center";
+          shadow_passes = 1;
+        }
+      ];
+    };
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    bindr = [ "$mainMod, L, exec, ${./hyprlock.sh}" ];
+    workspace = [
+      "name:hyprlock, monitor:eDP-1"
+      "name:hyprlock, decorate:false"
+      "name:hyprlock, border:false"
+      "name:hyprlock, shadow:false"
+      "special:hyprlock, monitor:eDP-1"
+      "special:hyprlock, decorate:false"
+      "special:hyprlock, border:false"
+      "special:hyprlock, shadow:false"
+    ];
+    windowrulev2 = [
+      "noblur, onworkspace:special:hyprlock"
+    ];
+    decoration.blur.special = true;
+  };
+
+  home.packages = with pkgs; [
+    alacritty
+    cava
+    cmatrix
+    asciiquarium-transparent
+  ];
+}
+
---
+++ modules/home-manager/wm/submodules/test_hyprlock_with_cava/hyprlock.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+asciiquarium="alacritty -e asciiquarium -t"
+cava="alacritty -o window.opacity=0 -e cava"
+cmatrix="alacritty -o window.opacity=0 font.size=15 -e cmatrix -u 12 -C magenta"
+
+pkill -USR1 waybar
+hyprctl dispatch focusmonitor eDP-1
+hyprctl dispatch workspace name:hyprlock
+hyprctl dispatch togglespecialworkspace hyprlock
+hyprctl dispatch "exec [float; size 100% 100%; move 0 0] $asciiquarium"
+hyprctl dispatch "exec [float; size 960 300; move 25% 100%-280] $cava"
+# hyprctl dispatch "exec [float; size 20% 100%; move 0 0] $cmatrix"
+# sleep 0.5
+# hyprctl dispatch "exec [float; size 20% 100%; move 80% 0] $cmatrix"
+hyprlock --immediate
+pkill -f "$asciiquarium"
+pkill -f "$cava"
+# pkill -f "$cmatrix"
+hyprctl dispatch workspace previous
+pkill -USR1 waybar
