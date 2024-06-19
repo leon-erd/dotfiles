3b13faf3f2db04363619ed640383bc3215ccbffc
Leon <leon.erd@student.uibk.ac.at>
2024-06-19 13:12:23 +0200

    firefox: restore old download behaviour + automatically enable extensions + add bookmark

--- modules/home-manager/apps/submodules/firefox.nix
+++ modules/home-manager/apps/submodules/firefox.nix
@@ -12,7 +12,11 @@
         "browser.startup.page" = 3;
         "signon.rememberSignons" = false;
         "extensions.activeThemeID" = "firefox-compact-light@mozilla.org";
+        "extensions.autoDisableScopes" = 0;
         "browser.toolbars.bookmarks.visibility" = "always";
+        "browser.download.always_ask_before_handling_new_types" = true;
+        "browser.download.start_downloads_in_tmp_dir" = true;
+        "browser.download.useDownloadDir" = false;
       };
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         betterttv
@@ -33,6 +37,8 @@
              url = "https://10.10.10.100";}
             {name = "Pihole";
              url = "http://10.10.10.100:8080/admin/";}
+            {name = "Github";
+             url = "https://github.com/";}
             {name = "Twitch";
              url = "https://twitch.tv/directory/following";}
             {name = "Youtube";
