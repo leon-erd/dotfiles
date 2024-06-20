821d5b236d920c1a6096cc171969f429a9bc8529
Leon <leon.erd@student.uibk.ac.at>
2024-06-20 16:14:10 +0200

    gammastep/location provider: Mozilla Location Service retired, disable geoclue for now and use manual location

--- modules/home-manager/wm/submodules/gammastep.nix
+++ modules/home-manager/wm/submodules/gammastep.nix
@@ -3,7 +3,10 @@
 {
   services.gammastep = {
     enable = true;
-    provider = "geoclue2";
+#     provider = "geoclue2"; # https://github.com/NixOS/nixpkgs/issues/321121
+    provider = "manual";
+    latitude = 47.3;
+    longitude = 11.4;
     tray = true;
   };
 }
