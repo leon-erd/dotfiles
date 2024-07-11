92686edbf70e41358fb63ad12373ef27c39ce4d1
Leon <leon.erd@student.uibk.ac.at>
2024-07-11 13:17:55 +0200

    xdg-desktop-portal: add WIP support for remotedesktop/KDEConnect

--- modules/system/wm/hyprland.nix
+++ modules/system/wm/hyprland.nix
@@ -15,6 +15,20 @@
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
+  xdg.portal = {
+    extraPortals = [
+      (pkgs.xdg-desktop-portal-wlr.overrideAttrs (previousAttrs: rec {
+        src = pkgs.fetchFromGitHub {
+          owner = "leon-erd";
+          repo = "xdg-desktop-portal-wlr";
+          rev = "424287fa0b2c59096e38534efdbd9a6bec13aead";
+          sha256 = "sha256-4igGdq8CpWbTmDVVKZwIs76eOTESDrGO5qa0m/BhYP4=";
+        };
+        buildInputs = previousAttrs.buildInputs ++ [ pkgs.libxkbcommon ];
+      }))
+    ];
+  };
+
   # Optional, hint electron apps to use wayland:
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
