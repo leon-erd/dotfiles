ffd3be53cb3a99b1f7ed1b5b3407c0ced0ddf56b
Leon <leon.erd@student.uibk.ac.at>
2024-07-11 11:55:46 +0200

    tofi: add rofi settings

--- modules/home-manager/wm/submodules/tofi.nix
+++ modules/home-manager/wm/submodules/tofi.nix
@@ -4,22 +4,33 @@
   programs.tofi = {
     enable = true;
     settings = {
-      anchor = "top";
-      width = "100%";
-      height = 36;
-      horizontal = true;
+      # General
+      font = "/run/current-system/sw/share/X11/fonts/NotoSansNerdFont-Regular.ttf";
       font-size = 14;
-      prompt-text = ''" run: "'';
-      outline-width = 0;
-      border-width = 0;
       background-color = "#00000099";
+      border-color = "#6400c8";
+      prompt-text = ''" run: "'';
       min-input-width = 120;
       result-spacing = 15;
-      padding-top = 5;
-      padding-bottom = 0;
-      padding-left = 0;
-      padding-right = 0;
-      font = "/run/current-system/sw/share/X11/fonts/NotoSansNerdFont-Regular.ttf";
+
+      # Dmenu
+#       anchor = "top";
+#       width = "100%";
+#       height = 36;
+#       horizontal = true;
+#       outline-width = 0;
+#       border-width = 0;
+#       padding-top = 5;
+#       padding-bottom = 0;
+#       padding-left = 0;
+#       padding-right = 0;
+
+      # Rofi
+      width = "50%";
+      height = "50%";
+      outline-width = 2;
+      border-width = 2;
+      corner-radius = 15;
     };
   };
 
