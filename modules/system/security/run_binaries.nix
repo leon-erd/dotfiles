8459a2a9a6346daadade1acc35d4d27523f6e50b
Leon <leonvincenterd@web.de>
2025-05-14 23:41:46 +0200

    run binaries: enable default libraries

--- modules/system/security/run_binaries.nix
+++ modules/system/security/run_binaries.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+# https://unix.stackexchange.com/questions/522822/different-methods-to-run-a-non-nixos-executable-on-nixos
+# https://github.com/thiagokokada/nix-alien
+{ pkgs, options, ... }:
 
 {
-  programs.nix-ld.enable = true;
-
-  programs.nix-ld.libraries = with pkgs; [
-    # Add any missing dynamic libraries for unpackaged programs here, NOT in environment.systemPackages
-  ];
+  programs.nix-ld = {
+    enable = true;
+    libraries = options.programs.nix-ld.libraries.default
+    ++ (with pkgs; [
+      # Add any missing dynamic libraries for unpackaged programs here, NOT in environment.systemPackages
+    ]);
+  };
 }
