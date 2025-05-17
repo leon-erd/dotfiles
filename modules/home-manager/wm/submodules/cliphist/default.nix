bf57eb21c1ffdf77637a702cb2b51c4b183991b0
Leon <leonvincenterd@web.de>
2025-05-17 22:31:03 +0200

    clipboard: copy primary selection into regular clipboard

--- modules/home-manager/wm/submodules/cliphist/default.nix
+++ modules/home-manager/wm/submodules/cliphist/default.nix
@@ -13,6 +13,7 @@
       "$mainMod, V, exec, cliphist list | rofi -dmenu -theme ${./spotlight-dark.rasi} | cliphist decode | wl-copy && wtype -M ctrl v -m ctrl"
     ];
     exec-once = [
+      "wl-paste --primary --type text --watch wl-copy" # copy primary clipboard (selected text) to regular clipboard (ctrg + c)
       "wl-paste --type text --watch cliphist store"
       "wl-paste --type image --watch cliphist store"
     ];
