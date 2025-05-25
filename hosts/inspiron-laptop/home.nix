57888b334bd8132f5aa05bb4eec9583dc2508691
Leon <leonvincenterd@web.de>
2025-05-26 01:39:08 +0200

    home-manager: enable xdg

--- hosts/inspiron-laptop/home.nix
+++ hosts/inspiron-laptop/home.nix
@@ -12,6 +12,7 @@
   # manage.
   home.username = userSettings.username;
   home.homeDirectory = "/home/${userSettings.username}";
+  xdg.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
