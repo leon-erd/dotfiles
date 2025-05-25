41e45dabbaafaaa130325d4b1e19a48831817f45
Leon <leonvincenterd@web.de>
2025-05-26 01:59:08 +0200

    shell: change aliases

--- modules/home-manager/devel/shell.nix
+++ modules/home-manager/devel/shell.nix
@@ -2,20 +2,18 @@
 
 let
   myAliases = {
-    pi = "ssh ubuntu@10.10.10.100";
-    get_temp = "paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'";
-    venv = "source venv/bin/activate";
     clearswap = "sudo swapoff -a; sudo swapon -a";
+    conda = "micromamba";
+    fhs = "nix-shell -E 'with import <nixpkgs> {}; (pkgs.buildFHSUserEnv { name = \"fhs\"; runScript = \"zsh\"; }).env'";
+    get_temp = "paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'";
+    homie = "nh home switch --configuration ${userSettings.userConfigurationName}";
+    nixie = "nh os switch --hostname ${userSettings.systemConfigurationName}";
     ls = "eza --icons --group-directories-first";
     ll = "eza --icons --group-directories-first --all --long";
     tree = "eza --icons --group-directories-first --tree";
-    conda = "micromamba";
-    homie = "nh home switch --configuration ${userSettings.userConfigurationName}";
-    nixie = "nh os switch --hostname ${userSettings.systemConfigurationName}";
-    vpn_pi_on = "wg-quick up ~/Nextcloud/Computer_current/pi_vpn.conf";
-    vpn_pi_off = "wg-quick down ~/Nextcloud/Computer_current/pi_vpn.conf";
-    neofetch = "fastfetch --config examples/7.jsonc";
-    fhs = "nix-shell -E 'with import <nixpkgs> {}; (pkgs.buildFHSUserEnv { name = \"fhs\"; runScript = \"zsh\"; }).env'";
+    neofetch = "nix run nixpkgs\#fastfetch -- --config examples/7.jsonc";
+    root_shell = "sudo env \"HOME=/home/$USER\" zsh --login";
+    venv = "source venv/bin/activate";
   };
 in
 {
