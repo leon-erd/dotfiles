af939caf3d634cf3ce8ef2d124211f1c1d6c9bfc
Leon <leonvincenterd@web.de>
2025-05-27 18:23:07 +0200

    shell: use custom oh-my-zsh robbyrussel theme

--- modules/home-manager/devel/shell.nix
+++ modules/home-manager/devel/shell.nix
@@ -47,6 +47,7 @@ in
     oh-my-zsh = {
       enable = true;
       theme = "robbyrussell";
+      custom = "$XDG_CONFIG_HOME/oh-my-zsh/custom";
       plugins = [
         "alias-finder"
         "command-not-found"
@@ -58,6 +59,18 @@ in
       ];
     };
   };
+
+  xdg.configFile."oh-my-zsh/custom/themes/robbyrussell.zsh-theme".text = ''
+    # custom robbyrussell theme
+    PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) %{$fg[cyan]%}%~%{$reset_color%}"
+    PROMPT+=' $(git_prompt_info)'
+    RPROMPT="%{$fg_bold[magenta]%}[%n@%M]%{$reset_color%}"
+
+    ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{✗%}"
+    ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+  '';
 
   programs.bash = {
     enable = true;
