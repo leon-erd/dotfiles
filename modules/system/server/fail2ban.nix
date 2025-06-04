6ca45b9f0081addbc0fa399489cd4aeabf7d4ec9
Leon <leonvincenterd@web.de>
2025-06-04 14:56:36 +0200

    fail2ban: add more default options

--- modules/system/server/fail2ban.nix
+++ modules/system/server/fail2ban.nix
@@ -3,9 +3,17 @@
 {
   services.fail2ban = {
     enable = true;
+    ignoreIP = [ ];
+    bantime = "10m";
+    maxretry = 5;
     bantime-increment = {
       enable = true;
       maxtime = "168h";
+    };
+    # defaults that cannot be set as module options go here (module options are transformed to DEFAULT.settings options)
+    jails.DEFAULT.settings = {
+      findtime = "10m";
+      dbpurgeage = "30d";
     };
   };
 }
