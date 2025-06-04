f02e99bc1aaf9ca3e80e02f743130023fdf322a7
Leon <leonvincenterd@web.de>
2025-06-04 22:42:00 +0200

    flake.lock: update

--- modules/home-manager/wm/submodules/swww.nix
+++ modules/home-manager/wm/submodules/swww.nix
@@ -5,7 +5,7 @@
   wayland.windowManager.hyprland.settings = {
     bind = [ "CTRL + ALT, W, exec, ~/scripts/wallpaper/update_wallpaper.sh" ];
     exec-once = [
-      "sleep 1 && swww init"
+      "sleep 1 && swww-daemon"
       "sleep 2 && swww img --transition-type grow ~/scripts/wallpaper/wallpaper.jpg"
     ];
   };
