a40691b7b25aaec1c15e1c2b8c8bfb46179a94cd
Leon <leonvincenterd@web.de>
2025-06-08 23:43:21 +0200

    hyprland: use flake portal package as per wiki

--- modules/system/wm/hyprland.nix
+++ modules/system/wm/hyprland.nix
@@ -13,7 +13,9 @@
 
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   xdg.portal = {
