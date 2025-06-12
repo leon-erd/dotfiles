b9ee0bb7cc64bd247b30e8cabc3c5873bfcfe11a
Leon <leonvincenterd@web.de>
2025-06-12 21:33:13 +0200

    scripts: add scripts for remote-deployment and install them as user programs as well as setting the corresponding env vars

--- hosts/inspiron-laptop/home.nix
+++ hosts/inspiron-laptop/home.nix
@@ -1,9 +1,24 @@
 {
   lib,
+  pkgs,
   inputs,
   userSettings,
   ...
 }:
+
+let
+  remote-deployment-home-script = builtins.readFile ../../scripts/remote-deployment-home.sh;
+  remote-deployment-system-script = builtins.readFile ../../scripts/remote-deployment-system.sh;
+
+  remoteDeploymentHome = pkgs.writeShellApplication {
+    name = "remote-deployment-home";
+    text = remote-deployment-home-script;
+  };
+  remoteDeploymentSystem = pkgs.writeShellApplication {
+    name = "remote-deployment-system";
+    text = remote-deployment-system-script;
+  };
+in
 
 {
   imports = [
@@ -21,6 +36,19 @@
     ../../modules/home-manager/devel/vscode
     ../../modules/home-manager/theming/theming.nix
     ../../modules/home-manager/wm/hyprland # needs hyprland.nix import in configuration.nix
+  ];
+
+  home.sessionVariables = {
+    DEPLOY_FLAKE = "${userSettings.flakeDirectory}/hosts/raspberrypi";
+    DEPLOY_USER = "leon";
+    DEPLOY_HOST = "raspberry.pi";
+    DEPLOY_USER_CONFIG_NAME = "leon@raspberrypi";
+    DEPLOY_SYSTEM_CONFIG_NAME = "raspberrypi";
+  };
+
+  home.packages = [
+    remoteDeploymentHome
+    remoteDeploymentSystem
   ];
 
   home.activation = {
---
+++ scripts/remote-deployment-system.sh
@@ -0,0 +1,34 @@
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# --- Color Variables ---
+COLOR='\e[94m'
+NC='\e[0m' # No Color (to reset)
+
+# --- Configuration ---
+# Prioritize command-line args, but fall back to env vars.
+flakedir="${1:-$DEPLOY_FLAKE}"
+configname="${2:-$DEPLOY_SYSTEM_CONFIG_NAME}"
+user="${3:-$DEPLOY_USER}"
+hostname="${4:-$DEPLOY_HOST}"
+
+# After attempting to set them, check if they are still empty.
+if [ -z "$flakedir" ] || [ -z "$configname" ] || [ -z "$user" ] || [ -z "$hostname" ]; then
+  echo "❌ Error: flakedir/configname/user/hostname not specified."
+  echo "Provide them as arguments or set env variables."
+  echo ""
+  echo "Usage (Args): $0 <flakedir> <configname> <user> <hostname>"
+  echo "Usage (Envs): DEPLOY_FLAKE=~/dots DEPLOY_CONFIG_NAME=leon-pi ..."
+  exit 1
+fi
+
+remote_host="${user}@${hostname}"
+flake_target="${flakedir}#${configname}"
+
+# --- Script Logic ---
+
+echo -e "${COLOR}Building system configuration '${configname}'${NC}"
+
+nixos-rebuild switch --flake "${flake_target}" --target-host "${remote_host}" --use-remote-sudo
+
+echo -e "${COLOR}✅ Deployment to ${remote_host} successful!${NC}"
