72e5a67656475c34da22dce0ce7527ae6e5c7c9a
Leon <leonvincenterd@web.de>
2025-06-19 01:08:11 +0200

    cava: use hm module

--- modules/home-manager/wm/hyprland/hyprlock_with_apps/default.nix
+++ modules/home-manager/wm/hyprland/hyprlock_with_apps/default.nix
@@ -110,8 +110,22 @@ in
 
   home.packages = with pkgs; [
     alacritty
-    cava
     cmatrix
     asciiquarium-transparent
   ];
+
+  programs.cava = {
+    enable = true;
+    settings.color = {
+      gradient = 1;
+      gradient_color_1 = "'#59cc33'";
+      gradient_color_2 = "'#80cc33'";
+      gradient_color_3 = "'#a6cc33'";
+      gradient_color_4 = "'#cccc33'";
+      gradient_color_5 = "'#cca633'";
+      gradient_color_6 = "'#cc8033'";
+      gradient_color_7 = "'#cc5933'";
+      gradient_color_8 = "'#cc3333'";
+    };
+  };
 }
