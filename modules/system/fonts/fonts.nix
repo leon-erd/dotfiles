81952903677409d1d5409e001a4283a7ceb7c083
Leon <leonvincenterd@web.de>
2025-06-26 15:14:05 +0200

    fonts: remove iosevka fonts

--- modules/system/fonts/fonts.nix
+++ modules/system/fonts/fonts.nix
@@ -1,9 +1,19 @@
 { pkgs, lib, ... }:
 
+# install all nerd fonts except Iosevka and its variants because they are huge and not needed
+let
+  allNerdFontsNames = builtins.attrNames pkgs.nerd-fonts;
+  dropIosevka = builtins.filter (name: lib.hasPrefix "iosevka" name) allNerdFontsNames;
+  customNerdFonts = builtins.removeAttrs pkgs.nerd-fonts dropIosevka;
+  customNerdFontsDerivations = builtins.filter lib.attrsets.isDerivation (
+    builtins.attrValues customNerdFonts
+  );
+in
+
 {
   fonts.packages = [
 
-  ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+  ] ++ customNerdFontsDerivations;
 
   fonts.fontDir.enable = true;
 }
