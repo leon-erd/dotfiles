11d7ac06a279660810d372e350289038ba8db212
Leon <leonvincenterd@web.de>
2025-07-02 16:42:07 +0200

    hardware/intel: add packages for OpenCL support

--- modules/system/hardware/intel.nix
+++ modules/system/hardware/intel.nix
@@ -6,6 +6,12 @@
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+
+      # OpenCL support (needed for hashcat)
+      intel-ocl
+      intel-compute-runtime
+      pocl
+      ocl-icd
     ];
   };
 
