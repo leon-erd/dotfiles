701280c5b7e6cfba57c68c7339357b5b6ba57542
Leon <leonvincenterd@web.de>
2025-09-15 15:55:27 +0200

    kde: disable setting kde as default session for display manager

--- modules/system/wm/kde.nix
+++ modules/system/wm/kde.nix
@@ -6,4 +6,6 @@
   ];
 
   services.desktopManager.plasma6.enable = true;
+  # the plasma6 module sets itself as the default session which I don't want
+  services.displayManager.defaultSession = null;
 }
