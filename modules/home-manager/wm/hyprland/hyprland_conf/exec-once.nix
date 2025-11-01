3349093fc46f44b0e35edf2324b63ad7756b3f9d
Leon Erd <leon.erd@zollsoft.de>
2025-11-01 20:01:55 +0100

    hyprland: exec scripts from /nix/store

--- modules/home-manager/wm/hyprland/hyprland_conf/exec-once.nix
+++ modules/home-manager/wm/hyprland/hyprland_conf/exec-once.nix
@@ -4,8 +4,8 @@
   wayland.windowManager.hyprland.settings = {
     # Execute at launch
     exec-once = [
-      "sleep 5 && ~/scripts/hibernate_on_low_battery.sh"
-      "sleep 5 && ~/scripts/restart_failed_systemd_user_services.sh"
+      "sleep 5 && ${../../../../../scripts/hibernate_on_low_battery.sh}"
+      "sleep 5 && ${../../../../../scripts/restart_failed_systemd_user_services.sh}"
     ];
   };
 }
