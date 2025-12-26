0709c9da05da2821fa3211b35ccfdc1743612de4
Leon <leonvincenterd@web.de>
2025-12-26 23:47:36 +0100

    sops: move ssh key setup to sops module

--- modules/system/basic/sops.nix
+++ modules/system/basic/sops.nix
@@ -6,4 +6,16 @@
     defaultSopsFile = ../../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
   };
+
+  # set up ssh keys for users
+  sops.secrets = builtins.listToAttrs (
+    builtins.map (user: {
+      name = "ssh/private_keys/${user.username}@${systemSettings.hostname}";
+      value = {
+        owner = user.username;
+        mode = "600";
+        path = "/home/${user.username}/.ssh/id_ed25519";
+      };
+    }) (builtins.attrValues systemSettings.users)
+  );
 }
--- modules/system/basic/users.nix
+++ modules/system/basic/users.nix
@@ -16,16 +16,4 @@
       };
     }) (builtins.attrValues systemSettings.users)
   );
-
-  # set up ssh keys for users
-  sops.secrets = builtins.listToAttrs (
-    builtins.map (user: {
-      name = "ssh/private_keys/${user.username}@${systemSettings.hostname}";
-      value = {
-        owner = user.username;
-        mode = "600";
-        path = "/home/${user.username}/.ssh/id_ed25519";
-      };
-    }) (builtins.attrValues systemSettings.users)
-  );
 }
