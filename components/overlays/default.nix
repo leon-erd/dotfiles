6ee574bcf239d585f0d99a3bae3a0816edc6beb1
Leon Erd <leon.erd@zollsoft.de>
2026-08-26 01:20:39 +0200

    overlays: update autoraie

--- components/overlays/default.nix
+++ components/overlays/default.nix
@@ -12,23 +12,15 @@
           propagatedUserEnvPkgs = [ ];
         });
 
-    autoraise = prev.autoraise.overrideAttrs {
+    autoraise = (prev.autoraise.override { enableExperimentalFocusFirst = true; }).overrideAttrs rec {
       version = "5.6";
 
       src = prev.fetchFromGitHub {
         owner = "sbmpost";
         repo = "AutoRaise";
-        rev = "304b05d18a8aa71dc95aa94ed80eab644bcbf701";
-        hash = "sha256-QmKGptrqzv7PKNpBVZVZhkJwA5U4ir3m21Hw3Kq2FYM=";
+        rev = "v${version}";
+        hash = "sha256-DQyXHZPM/5rt6Vhmyhb/ienvk0ZXzg6zbVAmUYeaOVA=";
       };
-
-      # build with EXPERIMENTAL_FOCUS_FIRST to focusing the hovered window before actually raising it
-      buildPhase = ''
-        runHook preBuild
-        $CXX -std=c++03 -fobjc-arc -D"NS_FORMAT_ARGUMENT(A)=" -D"SKYLIGHT_AVAILABLE=1" -DEXPERIMENTAL_FOCUS_FIRST -o AutoRaise AutoRaise.mm -framework AppKit -framework SkyLight
-        bash create-app-bundle.sh
-        runHook postBuild
-      '';
     };
   };
 }
